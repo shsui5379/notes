e81c1b1844b1d36fd18768393a284e6549590e4e
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-06-17 00:17:02 -0400

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var deploy = require('gulp-gh-pages');
 
-gulp.task('deploy', function () {
+gulp.task('publish', function () {
   return gulp.src("./_book/**/*")
     .pipe(deploy({
       remoteUrl: "https://github.com/shsui5379/notes.git",
